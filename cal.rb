cd8c1945571406ee690c49e21b7bf6ac577708f3
Matt Lummus <matthew.w.lummus@gmail.com>
2014-04-17 15:20:17 -0500

    Zeller tests passing, beginning work on calendar print

--- cal.rb
+++ cal.rb
@@ -8,6 +8,7 @@ month = input[0].dup
 year = input[1].dup
 month.chomp!
 year.chomp!
+year = year.to_i
 
 #in refactor stage, use regex to find abbreviations for months
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -26,27 +27,47 @@ end
 h = ""
 q = 1
 m = month.to_i
-K = year[2,3].to_i
-J = year[0,2].to_i
+k = year%100
+j = year/100
+y = year.to_i
 #puts "m: #{m}, K: #{K}, J: #{J}"
 
-#m = 13 if m == 01
-#m = 14 if m == 02
+if m < 3
+  y -= 1
+  m += 12
+  h = ( (q + (((m+1)*26)/10) + y + (y/4) + (6*(y/100)) + (y/400)) % 7 )
+else
+  h = ( (q + ((13*(m+1))/5) + k + (k/4) + (j/4) + (5*j) ) % 7 )
+end
 
-h = ( (q + ((13*(m+1))/5) + K + (K/4) + (J/4) + (5*J) ) % 7 )
 
-
-#PRINT----------------------------------------------------------------
+#PRINT 1ST DAY OF MONTH----------------------------------------------------------------
 #in refactor stage, use regex to find abbreviations for months
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days = [ "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 temp_day = days[h]
-temp_month = months[q-1]
+m -= 12 if m > 12
+temp_month = months[m-1]
 print "Your date is #{temp_day}, #{temp_month} #{q}, #{year}"
 
+#PRINT MONTH CALENDAR----------------------------------------------------------------
+=begin
+    January 2012
+20spaces
+11111111111111111111
+2char + 1 space
+Su Mo Tu We Th Fr Sa
+1char1space||2char + 1 spaces
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
 
 
 
+
+=end
 
 #NOTE FOR ZELLERS CONGRUENCE-------------
 #test case = Sunday, January 1st, 2012
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -3,9 +3,24 @@ require "minitest/autorun"
 
 class TestCal < MiniTest::Unit::TestCase
 
+  def test_format_cal_01_2012
+    shell_output = ""
+    IO.popen('ruby cal.rb 01 2012', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = <<EOS
+    January 2012
+Su Mo Tu We Th Fr Sa
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+
+=begin
 #FORMAT TESTS--------------------------------------
   #naive input
-  def test_forma_1_2012
+  def test_format_1_2012
     shell_output = ""
     IO.popen('ruby cal.rb 1 2012', 'r+') do |pipe|
       pipe.close_write
@@ -38,19 +53,164 @@ class TestCal < MiniTest::Unit::TestCase
   end
 
 
-#EDGE CASE TESTS-------------------------------------
-# 01-2012, 02-2012, 12-2012, 01-2014, 03-2014, 02-2015, 12-2015 01-1800, 02-1800, 07-1800, 12-1800, 01-1803, 02-1803
+#ZELLER'S CONGRUENCE TESTS-------------------------------------
+# 01-2012, 02-2012, 12-2012, 01-2014, 02-2014, 03-2014, 02-2015, 12-2015 01-1800, 02-1800, 07-1800, 12-1800, 01-1803, 02-1803, 12-1803
 
-  def test_zeller_0_2000
+#2012
+  def test_zeller_01_2012
     shell_output = ""
-    IO.popen('ruby cal.rb 08 2000', 'r+') do |pipe|
+    IO.popen('ruby cal.rb 01 2012', 'r+') do |pipe|
       pipe.close_write
       shell_output = pipe.read
     end
-    expected_output = "Your date is Tuesday, August 1, 2000"
+    expected_output = "Your date is Sunday, January 1, 2012"
     assert_equal expected_output, shell_output
   end
 
+  def test_zeller_02_2012
+    shell_output = ""
+    IO.popen('ruby cal.rb 02 2012', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Wednesday, February 1, 2012"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_12_2012
+    shell_output = ""
+    IO.popen('ruby cal.rb 12 2012', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Saturday, December 1, 2012"
+    assert_equal expected_output, shell_output
+  end
+
+#2014
+  def test_zeller_01_2014
+    shell_output = ""
+    IO.popen('ruby cal.rb 01 2014', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Wednesday, January 1, 2014"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_02_2014
+    shell_output = ""
+    IO.popen('ruby cal.rb 02 2014', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Saturday, February 1, 2014"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_03_2014
+    shell_output = ""
+    IO.popen('ruby cal.rb 03 2014', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Saturday, March 1, 2014"
+    assert_equal expected_output, shell_output
+  end
+
+#2015
+  def test_zeller_02_2015
+    shell_output = ""
+    IO.popen('ruby cal.rb 02 2015', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Sunday, February 1, 2015"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_12_2015
+    shell_output = ""
+    IO.popen('ruby cal.rb 12 2015', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Tuesday, December 1, 2015"
+    assert_equal expected_output, shell_output
+  end
+
+#1800
+  def test_zeller_01_1800
+    shell_output = ""
+    IO.popen('ruby cal.rb 01 1800', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Wednesday, January 1, 1800"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_02_1800
+    shell_output = ""
+    IO.popen('ruby cal.rb 02 1800', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Saturday, February 1, 1800"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_07_1800
+    shell_output = ""
+    IO.popen('ruby cal.rb 07 1800', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Tuesday, July 1, 1800"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_12_1800
+    shell_output = ""
+    IO.popen('ruby cal.rb 12 1800', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Monday, December 1, 1800"
+    assert_equal expected_output, shell_output
+  end
+
+#1803
+  def test_zeller_01_1803
+    shell_output = ""
+    IO.popen('ruby cal.rb 01 1803', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Saturday, January 1, 1803"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_02_1803
+    shell_output = ""
+    IO.popen('ruby cal.rb 02 1803', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Tuesday, February 1, 1803"
+    assert_equal expected_output, shell_output
+  end
+
+  def test_zeller_12_1803
+    shell_output = ""
+    IO.popen('ruby cal.rb 12 1803', 'r+') do |pipe|
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    expected_output = "Your date is Thursday, December 1, 1803"
+    assert_equal expected_output, shell_output
+  end
+=end
 
 #end of tests
 end
